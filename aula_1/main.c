2d6e784ceb1cf2616ee502d1abdc54536ada3e90
Wendell Costa <wendellcosta.dev@gmail.com>
2024-03-29 16:35:08 -0300

    Hello World :)

---
+++ aula_1/main.c
@@ -0,0 +1,16 @@
+//Primeiro é necessário instalar o compilador em C usando o minGW.
+
+#include <stdio.h>
+
+/*
+Este programa em C calcula a média do aluno seguindo o seguinte algoritmo:
+- Obtém a nota de cada uma das 4 provas.
+- Calcula a média das notas do aluno.
+- Se a média for igual ou maior que 7, o aluno foi aprovado, senão ele foi reprovado.
+*/
+
+int main(void)
+{
+    printf("Hello World!");
+    return 0;
+}
