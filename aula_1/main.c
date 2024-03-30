9290b1cf738914dea6661bc0c55d752c016a95c4
Wendell Costa <wendellcosta.dev@gmail.com>
2024-03-30 18:41:20 -0300

    Calculadora de média em linguagem C concluída

--- aula_1/main.c
+++ aula_1/main.c
@@ -1,4 +1,4 @@
-//Primeiro é necessário instalar o compilador em C usando o minGW.
+// Primeiro é necessário instalar o compilador em C usando o minGW.
 
 #include <stdio.h>
 
@@ -11,6 +11,31 @@ Este programa em C calcula a média do aluno seguindo o seguinte algoritmo:
 
 int main(void)
 {
-    printf("Hello World!");
+    // Declaração de variáveis.
+    float nota1, nota2, nota3, nota4, media;
+
+    // Obtém as notas do usuário.
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+    printf("Digite a quarta nota: ");
+    scanf("%f", &nota4);
+
+    // Calcula a média de notas.
+    media = nota1 + nota2 + nota3 + nota4 / 4;
+
+    // Verifica se o aluno foi aprovado ou não.
+    if (media >= 7)
+    {
+        printf("Você foi aprovado!");
+    }
+    else
+    {
+        printf("Infelizmente você foi reprovado");
+    }
+
     return 0;
 }
